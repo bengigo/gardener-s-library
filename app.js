8af0499c684749c57f9be9737009b7351e54c113
bengigenc <genc.bengi@gmail.com>
2022-06-24 17:39:56 +0300

    Create a book object constructor

---
+++ app.js
@@ -0,0 +1,9 @@
+// WHAT TO DO? WHAT TO DO?
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+        this.id = Date.now().toString();
+    }
+}
