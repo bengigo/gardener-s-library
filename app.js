a6f5483ee9e56f8b5a64a1dc94b6f283f9af83ef
bengigenc <genc.bengi@gmail.com>
2022-06-25 01:33:38 +0300

    Fix linter issues

--- app.js
+++ app.js
@@ -2,29 +2,6 @@
 
 // first thing sholud be display
 
-// need to ADD books
-// with an event
-// when the add button is clicked
-// const title = document.querySelector('#title-input').value;
-// const author = document.querySelector('#author-name').value;
-// let book = new Book(title, author);
-// let gardenBooks = [];
-// gardenBooks.push(book);
-// // store the gardenBooks data to local storage immidiately
-// store();
-
-// need to DELETE books
-// with an event
-// when the delete button is clicked
-// get the stored data from local storage to gardenBooks array
-
-// remove the selected book. to do so, use filter() method instead of removeChild() method, because filter() was recommended
-// like so >>> array = array.filter((item) => item.id !== event.targer.id);
-// update storage
-// may need to reload the page to completely update display
-
-// need to STORE books
-
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -34,7 +11,8 @@ class Book {
 
   static display() {
     const bookList = document.querySelector('#book-list');
-    const gardenBooks = JSON.parse(localStorage.getItem('gardenBooks'));
+    let gardenBooks = [];
+    gardenBooks = gardenBooks.concat(JSON.parse(localStorage.getItem('gardenBooks') || '[]'));
     gardenBooks.forEach((book) => {
       bookList.innerHTML += `
             <div class="book">
@@ -45,31 +23,13 @@ class Book {
     });
   }
 
-  static add() {
-    const addButton = document.querySelector('#add-button');
-    addButton.addEventListener('click', (e) => {
-      const title = document.querySelector('#title-input').value;
-      const author = document.querySelector('#author-name').value;
-      if (title !== '' && author !== '') {
-        e.preventDefault();
-        const book = new Book(title, author);
-        const gardenBooks = [];
-        gardenBooks.push(book);
-        Book.store();  
-      } else {
-        e.preventDefault();
-        const errorMessage = document.querySelector('#alert');
-        errorMessage.textContent = 'Please fill every filed.'
-      }
-    });
-  }
-
   static delete() {
     const deleteButton = document.querySelectorAll('.delete');
     deleteButton.forEach((button) => {
       button.addEventListener('click', (e) => {
-        let gardenBooks = [];
-        gardenBooks = JSON.parse(localStorage.getItem('gardenBooks'));
+        (e).preventDefault();
+        console.log('hello');
+        let gardenBooks = JSON.parse(localStorage.getItem('gardenBooks'));
         gardenBooks = gardenBooks.filter((book) => book.id !== e.target.id);
         localStorage.setItem('gardenBooks', JSON.stringify(gardenBooks));
         window.location.reload();
@@ -78,6 +38,40 @@ class Book {
   }
 
   static store() {
-
+    let gardenBooks = [];
+    const title = document.querySelector('#title-input').value;
+    const author = document.querySelector('#author-input').value;
+    if (title != '' && author != '') {
+      const book = new Book(title, author);
+      gardenBooks.push(book);
+      gardenBooks = gardenBooks.concat(JSON.parse(localStorage.getItem('gardenBooks') || '[]'));
+      localStorage.setItem('gardenBooks', JSON.stringify(gardenBooks));
+    }
   }
 }
+
+const addButton = document.querySelector('#add-button');
+const title = document.querySelector('#title-input');
+const author = document.querySelector('#author-input');
+
+addButton.addEventListener('click', (e) => {
+  console.log('hey');
+  e.preventDefault();
+  if (title.value == '' || author.value == '') {
+    e.preventDefault();
+    const errorMessage = document.querySelector('#alert');
+    errorMessage.textContent = 'Please fill every filed.';
+  } else {
+    e.preventDefault();
+    Book.store();
+    title.value = '';
+    author.value = '';
+    window.location.reload();
+  }
+});
+
+Book.display();
+
+Book.delete();
+
+Book.store();
